0971a79465bd207c208cecc4725db82e9c5130a2
Farhannazir725 <141568513+Farhannazir725@users.noreply.github.com>
2024-03-03 13:34:16 +0400

    check

---
+++ content.php
@@ -0,0 +1,34 @@
+<div class="body">
+<div class="terms-box">
+        <div class="terms-text">
+            <h2>Terms & Condition</h2>
+            <p>Greeting Users,</p>
+            <p>Our mission is to connect the users to allow them to be more productive and successful. Our services are designed to promote learning opportunity for our members by enabling you and millions of other professionals to meet, exchange ideas, learn, and find opportunities and learn in a network of trusted relationships.</p>
+
+            <p>When you register and join Forensic Mart or become a registered user, you become a Member. If you have chosen not to register for our Services, you may access certain features as a “Visitor.”</p>
+
+            <p>We may modify the terms and our Privacy Policy from time to time. If we make material changes to it, we will provide you notice through our Services, or by other means, to provide you the opportunity to review the changes before they become effective. We agree that changes cannot be retroactive. If you object to any changes, you may close your account. Your continued use of our Services after we publish or send a notice about our changes to these terms means that you are consenting to the updated terms.</p>
+
+            <p>We help you find and connect with your people, groups, businesses, organizations and other things that matter to you. We recommend to you or to other people using the available data.</p>
+
+            <p>We show you ads, offers, and other sponsored content/information/contents so that you can find the materials/information/contents, products and services offered by many businesses and organizations using Forensic Mart. Our partners pay us to show you their content/information/content, and we design our services so that you can see the sponsored content/information/content you see, as well as all the other things that you see on our products as relevant and be useful.</p>
+
+            <p>Members are account holders. You agree to: (1) try to choose a strong and secure password; (2) keep your password secure and confidential; (3) not transfer any part of your account (e.g., connections). You are responsible for anything that happens through your account unless you close it or report misuse</p>
+
+                                                            <!-- Google Ads -->
+
+            <p>We may change or discontinue any of our Services. We don’t promise to store or keep showing any information and content that you’ve posted.</p>
+
+            <p>Forensic Mart is not a storage service. You agree that we have no obligation to store, maintain or provide you with a copy of any content or information that you or others provide, except to the extent required by applicable law and as noted in our Privacy Policy.
+             </p>
+             <p>   
+TO THE EXTENT ALLOWED UNDER LAW, FORENSIC MART (A) DISCLAIM ALL IMPLIED WARRANTIES AND REPRESENTATIONS (E.G. WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, ACCURACY OF DATA, AND NONINFRINGEMENT); (B) DO NOT GUARANTEE THAT THE SERVICES WILL FUNCTION WITHOUT INTERRUPTION OR ERRORS, AND (C) PROVIDE THE SERVICE (INCLUDING CONTENT AND INFORMATION) ON AN “AS IS” AND “AS AVAILABLE” BASIS</p>
+
+            <p>TO THE EXTENT PERMITTED UNDER LAW, FORENSIC MART SHALL NOT BE LIABLE TO YOU OR OTHERS FOR ANY INDIRECT, INCIDENTAL, SPECIAL, CONSEQUENTIAL OR PUNITIVE DAMAGES, OR ANY LOSS OF DATA, OPPORTUNITIES, REPUTATION, PROFITS OR REVENUES, RELATED TO THE SERVICES E.G. OFFENSIVE OR DEFAMATORY STATEMENTS, DOWNTIME OR LOSS, USE OF, OR CHANGES TO, YOUR INFORMATION OR CONTENT.</p>
+
+        </div>
+        <h4> I Agree to the <span>Terms & Condition</span> and I read the Privacy Notice.</h4>
+
+    </div>
+
+    </div>
---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content=Login/Register"width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Forensic Mart Term & Condition</title>
+    <style>
+        /* nav{
+        background-color: red;   
+        } */
+    </style>
+</head>
+<body>
+    <div class="logo" style="text-align: center; background-color: black; "><img src="logo.png" alt=""></div>
+    <div class="text" style="text-align: center; background-color: black;color: cyan; font-weight: 700; font-size: medium; ">Forensic Mart</div>
+    <nav>
+       <div class="first"></div>
+     
+       <div class="second">
+       <ul>
+            <li><a href="#">Login/Register </a></li>
+            <li><a href="#">About Forensic Mart </a></li>
+            <li><a href="#">Events </a></li>
+            <li><a href="#">Gallery </a></li>
+            <li><a href="#">Service </a></li>
+            <li><a href="#">Terms & Condition </a></li>
+            <li><a href="#">Privacy Policy </a></li>
+            <li><a href="#">Support </a></li>
+        </ul>
+       </div>
+      
+    </nav>
+   
+  
+
+<?php include 'content.php'; ?>    
+</body>
+</html>
